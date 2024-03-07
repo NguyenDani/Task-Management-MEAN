d914920c512c48740d6326a76d7cc91aa105e7ea
Daniel Nguyen <nguyendaniel1997@gmail.com>
2024-03-07 04:17:34 -0600

    Setup basic backend connect DB and start server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Start Server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
