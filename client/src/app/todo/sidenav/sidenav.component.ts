be632226ab9aa3de70f3fbcb5de792b254970b55
Daniel Nguyen <nguyendaniel1997@gmail.com>
2024-03-17 23:36:24 -0500

    Create task layout

--- client/src/app/todo/sidenav/sidenav.component.ts
+++ client/src/app/todo/sidenav/sidenav.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
   imports: [
     MatDividerModule,
-    MatListModule
+    MatListModule,
+    MatDialogModule,
+    MatButtonModule
   ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openCreateTask() {
+    const dialogRef = this.dialog.open(NewTaskComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
