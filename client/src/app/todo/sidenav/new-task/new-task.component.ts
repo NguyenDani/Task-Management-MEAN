5f4d0a5ec961f444658836f3f26ad2a2b1b6f956
Daniel Nguyen <nguyendaniel1997@gmail.com>
2024-03-18 23:56:43 -0500

    Implement material to create task

--- client/src/app/todo/sidenav/new-task/new-task.component.ts
+++ client/src/app/todo/sidenav/new-task/new-task.component.ts
@@ -1,12 +1,78 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule, FloatLabelType } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter, ThemePalette } from '@angular/material/core';
+import { MatCheckboxModule}  from '@angular/material/checkbox';
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  color: ThemePalette;
+  subtasks?: Task[];
+}
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatFormFieldModule, 
+    MatInputModule, 
+    FormsModule, 
+    ReactiveFormsModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+  ],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss'
 })
 export class NewTaskComponent {
+  title = new FormControl('', [Validators.required]);
+  status = new FormControl('TO_DO' as FloatLabelType);
 
+  // Title
+  getErrorMessage() {
+    if (this.title.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.title.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  // Tasks
+  task: Task = {
+    name: 'Indeterminate',
+    completed: false,
+    color: 'primary',
+    subtasks: [
+      {name: 'Primary', completed: false, color: 'primary'},
+      {name: 'Accent', completed: false, color: 'accent'},
+      {name: 'Warn', completed: false, color: 'warn'},
+    ],
+  };
+
+  allComplete: boolean = false;
+
+  updateAllComplete() {
+    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+  }
+
+  someComplete(): boolean {
+    if (this.task.subtasks == null) {
+      return false;
+    }
+    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    if (this.task.subtasks == null) {
+      return;
+    }
+    this.task.subtasks.forEach(t => (t.completed = completed));
+  }
 }
