eb0a5b60ab293c297964052c2c8db1f27c1ec569
Daniel Nguyen <nguyendaniel1997@gmail.com>
2024-04-10 18:03:52 -0500

    Fixed route and guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { TodoComponent } from './todo/todo.component';
-import { authGuard } from './auth.guard';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', component:AuthComponent },
-    { path: 'dashboard', component:TodoComponent, canActivate: [authGuard], redirectTo: '' },
-    { path: '', redirectTo: '', pathMatch: 'full' },
+    { path: 'dashboard', component:TodoComponent, canActivate: [authGuard] },
+    { path: '*', redirectTo: '', pathMatch: 'full' },
     { path: '**', redirectTo: '' }
 ];
--- client/src/app/auth.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivateFn, Router } from '@angular/router';
-
-export const authGuard: CanActivateFn = (route, state) => {
-  if(!localStorage.getItem('token')) {
-    return false;
-  }
-      
-  return true;
-};
